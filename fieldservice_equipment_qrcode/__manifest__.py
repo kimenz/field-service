8c53ea8e3f740a3e31755b3155669a3c5975005c
rafnix guzman <rafnixg@gmail.com>
2024-10-09 20:27:43 +0000

    ADD: labels for: CERTIFICADO CALIBRAÇÃO, SEGURANÇA ELETRICA, VISITA PREVENTIVA

--- fieldservice_equipment_qrcode/__manifest__.py
+++ fieldservice_equipment_qrcode/__manifest__.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Fieldservice Equipment Qrcode",
+    "name": "Fieldservice Equipment Labels and Qrcode",
     "summary": """
         This module is an add-on for the Field Service application in Odoo.
         It allows you go to your FSM Equipments print
-        QRcode with URL to fieldserice_equipment_website.""",
+        Labels and QRcode with URL to fieldserice_equipment_website.""",
     "version": "14.0.1.0.2",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/field-service",
